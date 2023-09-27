6aacd637d71a3a5b2e50e7918b6d7bdb2b3a9763
Navin <navinrawat11032004@gmail.com>
2023-09-27 15:16:41 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+"use client"
+import React from "react"
+import Login from "../pages/login/Login"
+
+ const Home = () => {
+  return (
+  <>
+    <Login />
+  </>
+  ) 
+}
+export default Home;
---
+++ pages/login/Login.js
@@ -0,0 +1,11 @@
+"use client"
+import React from 'react'
+import "./login.css"
+
+ const login = () => {
+  return (
+    <div className='w-[fix fit-content] bg-[blueviolet] relative text-center text-[150px] rounded-[10px]'>Login</div>
+  )
+}
+
+export default login;
